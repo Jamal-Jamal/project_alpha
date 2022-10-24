d394dee7520a2e0edfcb3ba7814f735bab543f40
Jamal <jamal.degeratto@gmail.com>
2022-10-24 18:39:18 -0500

    Feature 4 complete

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 
 
-# Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
