950c3e6c044f5e94b41f8bcbe028f94030997574
Jamal <jamal.degeratto@gmail.com>
2022-10-26 22:59:03 -0500

    Feature 15 complete

--- tasks/views.py
+++ tasks/views.py
@@ -5,12 +5,11 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def create_task(request):
-    if request.medthod == "POST":
+    if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
-            task = form.save()
-            task.save()
-            return redirect("list_project")
+            form.save()
+            return redirect("list_projects")
     else:
         form = TaskForm()
     context = {
